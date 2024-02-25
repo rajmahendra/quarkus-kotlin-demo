9c3f380ca5c933f24a75be71acbaeaaf71a38524
Rajmahendra Hegde <rajmahendra@gmail.com>
2024-02-25 05:46:44 +0530

    Cleanup of data

--- src/main/kotlin/in/rajmahendra/movie/resources/MovieResource.kt
+++ src/main/kotlin/in/rajmahendra/movie/resources/MovieResource.kt
@@ -1,40 +1,57 @@
 package `in`.rajmahendra.movie.resources
 
 import `in`.rajmahendra.movie.entity.Movie
+import jakarta.inject.Inject
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import org.jboss.logging.Logger
 
-@Path( "/book")
-@Produces (MediaType.APPLICATION_JSON)
+@Path("/movies")
+@Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-class MovieResource {
+class MovieResource @Inject constructor (val log: Logger) {
 
-    val movies: ArrayList<Movie> = ArrayList();
+ //   val log: Logger = Logger.getLogger(MovieResource::class.java)
+
+    final val movies: ArrayList<Movie> = ArrayList();
+
+    init {
+        movies.add(Movie(id = 1, title = "Ponnien Selvan", released = 2022))
+        movies.add(Movie(id = 2, title = "Ponnien Selvan 2", released = 2023))
+    }
 
     @POST
-    fun addMovie(movie: Movie) : Movie {
-         movies.add(movie);
+    fun addMovie(movie: Movie): Movie {
+        log.info("addMovie called")
+        movies.add(movie);
         return movie;
     }
 
     @GET
-    fun getAllMovie() = movies
+    fun getAllMovie(): ArrayList<Movie> {
+        log.info("getAllMovie Called")
+        log.info("Movie list size : " + movies.size)
+        return movies;
+    }
 
     @GET
-    @Path("/id")
-    fun getMovieById(id: Long) : Movie? {
-        val movie:Movie? = movies.find{ it.id == id}
+    @Path("/id/{id}")
+    fun getMovieById(id: Long): Movie? {
+        log.info("getMovieById called")
+        val movie: Movie? = movies.find { it.id == id }
+        log.info("Movie found in ID : $movie")
         return movie
     }
 
     @GET
-    @Path("/title")
-    fun getMovieByTitle(title: String) : Movie? {
-        val movie:Movie? = movies.find{ it.title == title}
+    @Path("/title/{title}")
+    fun getMovieByTitle(title: String): Movie? {
+        log.info("getMovieByTitle called")
+        val movie: Movie? = movies.find { it.title == title }
         return movie
     }
 
--- src/test/kotlin/in/rajmahendra/movie/TestMovie.kt
+++ src/test/kotlin/in/rajmahendra/movie/TestMovie.kt
@@ -1,4 +1,41 @@
 package `in`.rajmahendra.movie
 
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
 class TestMovie {
+    @Test
+    fun initiallyMoviesIsEmpty() {
+        given()
+            .`when`().get("/movies")
+            .then()
+            .statusCode(equalTo(200))
+    }
+
+    @Test
+    fun shouldBeEmptyWhenInvalidMovieIdProvided() {
+        given()
+            .pathParam("id", 2)
+            .`when`().get("/movies/id/{id}")
+            .then()
+            .statusCode(200).body("id", equalTo(2)).body("title", equalTo("Ponnien Selvan 2"))
+
+    }
+
+    @Test
+    fun shouldAddAMovie() {
+        val jsonStr = """{"id" : 3,"title" : "Sardar","released" : 2022}"""
+
+        given().contentType(ContentType.JSON).
+        and().body(jsonStr).
+        `when`().post("/movies").
+        then().body("id", equalTo(3))
+    }
+
+
 }
